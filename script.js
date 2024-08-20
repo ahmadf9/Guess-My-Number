712a093bcca33c5dd4ea53b1a04a1474e273c749
Ahmad Faizal <ahmadfaizalarrosyid@gmail.com>
2024-08-20 10:09:35 +0700

    Refactoring Dry Code

--- script.js
+++ script.js
@@ -6,6 +6,11 @@ let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
 let highscore = 0;
 
+// Refacoring function
+const displayMessage = function (message) {
+  document.querySelector('.message').textContent = message;
+};
+
 /* Menambahkan Event Listener Tombol 'Check' */
 document.querySelector('.check').addEventListener('click', function () {
   // Konversi String ke Number
@@ -14,11 +19,12 @@ document.querySelector('.check').addEventListener('click', function () {
 
   // SKENARIO: Ketika tidak ada input
   if (!guess) {
-    document.querySelector('.message').textContent = '🚫 No number!';
+    displayMessage('🚫 No number!'); // Memanggil Refactoring function
+    // document.querySelector('.message').textContent = '🚫 No number!';
 
     // SKENARIO: Ketika player menang
   } else if (guess === secretNumber) {
-    document.querySelector('.message').textContent = '🎉 Correct Number!';
+    displayMessage('🎉 Correct Number!');
     document.querySelector('.number').textContent = secretNumber;
     /* Manipulasi CSS Styles */
     document.querySelector('body').style.backgroundColor = '#60b347';
@@ -29,28 +35,41 @@ document.querySelector('.check').addEventListener('click', function () {
       document.querySelector('.highscore').textContent = highscore;
     }
 
-    // SKENARIO: Ketika tebakan terlalu tinggi
-  } else if (guess > secretNumber) {
-    // Game Over
+    // Tebakan salah / tidak sama
+  } else if (guess !== secretNumber) {
     if (score > 1) {
-      document.querySelector('.message').textContent = '📈 Too high!';
-      score--; // SKENARIO: Ketika tebakan salah, score -1.
-      document.querySelector('.score').textContent = score;
-    } else {
-      document.querySelector('.message').textContent = '😥 You lost the game!';
-      document.querySelector('.score').textContent = 0;
-    }
-    // SKENARIO: Ketika tebakan terlalu rendah
-  } else if (guess < secretNumber) {
-    if (score > 1) {
-      document.querySelector('.message').textContent = '📉 Too low!';
-      score--; // SKENARIO: Ketika tebakan salah, score -1.
+      document.querySelector('.message').textContent =
+        guess > secretNumber ? '📈 Too high!' : '📉 Too low!';
+      score--;
       document.querySelector('.score').textContent = score;
     } else {
       document.querySelector('.message').textContent = '😥 You lost the game!';
       document.querySelector('.score').textContent = 0;
     }
   }
+
+  // SKENARIO: Ketika tebakan terlalu tinggi
+  // } else if (guess > secretNumber) {
+  //   // Game Over
+  //   if (score > 1) {
+  //     document.querySelector('.message').textContent = '📈 Too high!';
+  //     score--; // SKENARIO: Ketika tebakan salah, score -1.
+  //     document.querySelector('.score').textContent = score;
+  //   } else {
+  //     document.querySelector('.message').textContent = '😥 You lost the game!';
+  //     document.querySelector('.score').textContent = 0;
+  //   }
+
+  // SKENARIO: Ketika tebakan terlalu rendah
+  // } else if (guess < secretNumber) {
+  //   if (score > 1) {
+  //     document.querySelector('.message').textContent = '📉 Too low!';
+  //     score--; // SKENARIO: Ketika tebakan salah, score -1.
+  //     document.querySelector('.score').textContent = score;
+  //   } else {
+  //     document.querySelector('.message').textContent = '😥 You lost the game!';
+  //     document.querySelector('.score').textContent = 0;
+  //   }
 });
 
 /* Implement a game reset functionality, so that the player can make a new guess! */
@@ -67,4 +86,3 @@ document.querySelector('.again').addEventListener('click', function () {
   document.querySelector('body').style.backgroundColor = '#222';
   document.querySelector('.number').style.width = '15rem';
 });
-///////////////////////////////////////
